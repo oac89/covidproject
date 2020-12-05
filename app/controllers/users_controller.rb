c733ff1e3a52f93f36975a1c4aa5bb5f283393b1
oac89 <oacastro2@gmail.com>
2020-12-05 16:43:51 -0600

    updated new#form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,8 @@ class UsersController < ApplicationController
     end 
 
     def create
-        @user = Post.new(user_params) 
-        if post.save
+        @user = User.new(user_params) 
+        if user.save
             redirect_to user_path(@user)
         else 
             render :new 
