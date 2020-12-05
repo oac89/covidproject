ad1b11bba2e92a96563de95d87beb21907d13427
oac89 <oacastro2@gmail.com>
2020-12-04 23:26:09 -0600

    23:25 update

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,18 +38,23 @@ Alert.destroy_all
 grocery_store = Location.create(name: "Tony's", address: "4894 W. Milwaukee Ave")
 bank = Location.create(name: "Bank of America", address: "7894 W. Damen Ave")
 coffee = Location.create(name: "Ipsento", address: "4613 N. California")
+gym = Location.create(name: "Plant Fitness", address: "7895 N. Diversy")
+taco = Location.create(name: "Taconazo", address: "3631 N. Central")
+mall = Location.create(name: "Brickyard Mall", address: "2530 N. Narragansett")
+
+# mon = Weekday.create(user_id: user.id, location_id: location.id, day: "Monday")
+# tue = Weekday.create(user_id: user.id, location_id: location.id, day: "Tuesday")
+# wed = Weekday.create(user_id: user.id, location_id: location.id, day: "Wednesday")
+# thurs = Weekday.create(user_id: user.id, location_id: location.id, day: "Thursday")
+# fri = Weekday.create(user_id: user.id, location_id: location.id, day: "Friday")
+# sat = Weekday.create(user_id: user.id, location_id: location.id, day: "Saturday")
+# sun = Weekday.create(user_id: user.id, location_id: location.id, day: "Sunday")
 
 
-
-mon = Weekday.create(user_id: user.id, location_id: location.id, day: "Monday")
-tue = Weekday.create(user_id: user.id, location_id: location.id, day: "Tuesday")
-wed = Weekday.create(user_id: user.id, location_id: location.id, day: "Wednesday")
-thurs = Weekday.create(user_id: user.id, location_id: location.id, day: "Thursday")
-fri = Weekday.create(user_id: user.id, location_id: location.id, day: "Friday")
-sat = Weekday.create(user_id: user.id, location_id: location.id, day: "Saturday")
-sun = Weekday.create(user_id: user.id, location_id: location.id, day: "Sunday")
-
-
-
+alert1 = Alert.create(type: "Possible exposure at a location you visited, quarantine and test ASAP!")
+alert2 = Alert.create(type: "Negative for Covid-19 at this time. Please keep practicing social distancing and covering your face including the nose!")
+alert3 = Alert.create(type: "You have indicated positive for Covid-19. Please adhere to recommended CDC guidelines and check with your physician.")
+alert4 = Alert.create(type: "Vaccinated.")
+alert5 = Alert.create(type: "Please indicate your status.")
 
 
